464a0c86b6847f6ec7ad6063387d1e137466d605
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-01-05 16:25:59 +0100

    added 4story

--- script.js
+++ script.js
@@ -33,6 +33,18 @@ const additionalCharacters = [
   },
 ];
 
+const history = [
+  {
+    description:
+      "Twoja odwaga niechaj będzie światłem w mroku, podróżniku. Oto zadanie, które czeka przed tobą i twoim bohaterem. W jednym z odległych zakątków świata istnieje starożytny świątynny kompleks, gdzie ukryto mistyczne artefakty. Twoim celem jest dotarcie tam i zdobycie tych skarbów, które posiadają moce, o jakich nie śniłbyś nawet w najbardziej odległych snach.",
+  },
+  {
+    description:
+      "Przekroczenie tego portalu otworzy przed wami nowe wyzwania i możliwości. Ale pamiętajcie, losy wasze są nadal splecione. Wybory, które podejmiecie, wpłyną na przyszłość nie tylko waszą, lecz i całego świata.",
+  },
+];
+let statGameFlag = false;
+
 const mainContainer = document.querySelector("main");
 
 const start = document.createElement("button");
@@ -42,11 +54,20 @@ start.addEventListener("click", startGame);
 mainContainer.appendChild(start);
 
 function startGame() {
+  let main;
   const h1 = document.querySelector("h1");
   h1.innerText = "Wybierz jedna z 3 postaci";
-  const main = document.getElementById("start-container");
-  // main.innerHTML = "";
-  mainContainer.removeChild(start);
+  if (!statGameFlag) {
+    main = document.getElementById("start-container");
+    main.innerHTML = "";
+    mainContainer.removeChild(start);
+    statGameFlag = true;
+  } else {
+    mainContainer.innerHTML = "";
+    main = document.createElement("main");
+    main.id = "start-container";
+    mainContainer.appendChild(main);
+  }
 
   characters.forEach((character) => {
     const div = document.createElement("div");
@@ -120,7 +141,13 @@ function pickCharacter(event) {
       const acceptBtn = document.createElement("button");
       acceptBtn.innerText = "Zaakceptuj wyzwanie";
       acceptBtn.classList.add("button");
-      acceptBtn.addEventListener("click", startGame);
+      acceptBtn.addEventListener("click", function () {
+        updateStory(
+          history[0].description,
+          history[1].description,
+          "Zaakceptuj Wyzwanie"
+        );
+      });
       btnContainer.appendChild(acceptBtn);
 
       const rejectBtn = document.createElement("button");
@@ -148,4 +175,57 @@ function typeWriter(element, text, delay = 5) {
     }
     helper();
   });
+}
+
+function updateStory(story1, story2, title) {
+  const h1 = document.querySelector("h1");
+  h1.innerText = title;
+  mainContainer.innerHTML = "";
+  mainContainer.classList.add("main-container");
+
+  const divStory = document.createElement("div");
+  divStory.classList.add("sleceted-container");
+  const divImg = document.createElement("img");
+  divImg.src = "/img/Księgowy Cienia.jpeg";
+  const h2 = document.createElement("h2");
+  h2.classList.add("introduction");
+  divStory.appendChild(divImg);
+  divStory.appendChild(h2);
+  mainContainer.appendChild(divStory);
+
+  const div2 = document.createElement("div");
+  div2.classList.add("sleceted-container");
+  const divImg2 = document.createElement("img");
+  divImg2.src = "/img/Księgowy Cienia.jpeg";
+  const h22 = document.createElement("h2");
+  h22.classList.add("introduction");
+
+  typeWriter(h2, story1)
+    .then(() => {
+      div2.appendChild(h22);
+      div2.appendChild(divImg2);
+      mainContainer.appendChild(div2);
+      return typeWriter(h22, story2);
+    })
+    .then(() => {
+      const btnContainer = document.createElement("div");
+      btnContainer.classList.add("button-container");
+
+      const acceptBtn = document.createElement("button");
+      acceptBtn.innerText = "Zaakceptuj wyzwanie";
+      acceptBtn.classList.add("button");
+      acceptBtn.addEventListener("click", function () {
+        updateStory(history[0].description, history[1].description);
+      });
+      btnContainer.appendChild(acceptBtn);
+
+      const rejectBtn = document.createElement("button");
+      rejectBtn.innerText = "Odrzuć wyzwanie";
+      rejectBtn.classList.add("button-reject");
+
+      rejectBtn.addEventListener("click", startGame);
+      btnContainer.appendChild(rejectBtn);
+
+      mainContainer.appendChild(btnContainer);
+    });
 }
